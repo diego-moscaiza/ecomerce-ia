d97bb55b9e18e4da902aaf19800b0c36476b177c
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-19 22:25:06 -0500

    API agregada en cada formulario. Usuarios añadidos a la base de datos. Adaptador SSR de Node.js añadido

--- tienda-frontend/astro.config.mjs
+++ tienda-frontend/astro.config.mjs
@@ -1,8 +1,22 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
+  integrations: [tailwind()],
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+      },
+    ],
+  },
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
