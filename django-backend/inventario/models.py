8cc645de839ef41925b3647f4580ae25dff628fa
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-26 22:36:49 -0500

    Modelo de la app inventario ha sido modificada

--- django-backend/inventario/models.py
+++ django-backend/inventario/models.py
@@ -2,6 +2,4 @@ from django.db import models
 
 class Inventario(models.Model):
     id_inventario = models.AutoField(primary_key=True)
-    ubicacion = models.CharField(max_length=100, null=True)
-    stock = models.IntegerField(default=0)
-    estado = models.IntegerField(default=0)
+    nombre = models.CharField(max_length=20)
