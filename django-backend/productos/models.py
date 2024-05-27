c61fb6477bedb3a38c2459c3f43a293c21a1776d
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-27 01:51:48 -0500

    App productos modificada

--- django-backend/productos/models.py
+++ django-backend/productos/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Producto(models.Model):
     id_producto = models.AutoField(primary_key=True)
-    # id_inventario = models.ForeignKey('inventario.Inventario', on_delete=models.CASCADE)
-    # id_proveedor = models.ForeignKey('proveedor.Proveedor', on_delete=models.CASCADE)
-    # id_categoria = models.ForeignKey('categoria.Categoria', on_delete=models.CASCADE)
+    id_proveedor = models.ForeignKey('proveedor.Proveedor', on_delete=models.CASCADE, default=0)
+    id_categoria = models.ForeignKey('categoria.Categoria', on_delete=models.CASCADE, default=0)
+    id_estante = models.ForeignKey('estante.Estante', on_delete=models.CASCADE, default=0)
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=100, blank=True)
     cantidad = models.IntegerField(default=0)
