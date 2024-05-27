efa4307aff19b1560b82effc69419d9209f21fbd
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-27 01:38:56 -0500

    Modificaciones a las tablas "clientes", "proveedor", "productos" de la base de datos.

--- django-backend/productos/models.py
+++ django-backend/productos/models.py
@@ -7,7 +7,7 @@ class Producto(models.Model):
     # id_proveedor = models.ForeignKey('proveedor.Proveedor', on_delete=models.CASCADE)
     # id_categoria = models.ForeignKey('categoria.Categoria', on_delete=models.CASCADE)
     nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=100, null=True)
+    descripcion = models.CharField(max_length=100, blank=True)
     cantidad = models.IntegerField(default=0)
     precio = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     estado = models.IntegerField(default=0)
