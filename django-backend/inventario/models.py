962e04d9ecaf54fbe83a393bb37197f66dc00908
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-26 19:58:48 -0500

    App inventario y su modelo han sido añadidos

--- django-backend/inventario/models.py
+++ django-backend/inventario/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Inventario(models.Model):
     id_inventario = models.AutoField(primary_key=True)
-    ubicacion = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=100, null=True)
     stock = models.IntegerField(default=0)
-    estado = models.CharField(max_length=1)
+    estado = models.IntegerField(default=0)
