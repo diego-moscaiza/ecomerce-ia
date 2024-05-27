2b54bab9300d6f8d171d12e5fa368584154d7733
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-26 23:40:59 -0500

    App categoria modificada en la base de datos

--- django-backend/categoria/models.py
+++ django-backend/categoria/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=60, null=True)
