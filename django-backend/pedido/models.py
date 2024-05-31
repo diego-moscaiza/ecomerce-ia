811c6e170908d931a4f739c308a8f25437060137
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-30 21:41:36 -0500

    Cambios añadidos en la app pedido

--- django-backend/pedido/models.py
+++ django-backend/pedido/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from clientes.models import Cliente
+from productos.models import Producto
 
-# Create your models here.
+class Pedido(models.Model):
+    id_pedido = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, blank=True, null=True)
+    id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE, blank=True, null=True)
+    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    estado = models.CharField(max_length=1)
