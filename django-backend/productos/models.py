26beddcaf362562d9994d6bffc871d16ee066939
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-31 11:50:33 -0500

    Modelo de la app productos ha sido modificada

--- django-backend/productos/models.py
+++ django-backend/productos/models.py
@@ -11,4 +11,4 @@ class Producto(models.Model):
     cantidad = models.IntegerField(default=0)
     precio = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     estado = models.IntegerField(default=0)
-    img = models.CharField(max_length=35)
+    img = models.CharField(max_length=35, null=True, blank=True)
