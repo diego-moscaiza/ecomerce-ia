c3b38fd66acbc2118a6943d3800ad0f49d42caf8
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-30 21:42:04 -0500

    Cambios añadidos en la app comprobante

--- django-backend/tipo_comprobante/models.py
+++ django-backend/tipo_comprobante/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class TipoComprobante(models.Model):
+    id_tipo_comprobante = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=25, blank=True, null=True)
