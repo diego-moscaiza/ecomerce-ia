b1f27b26a9e88036ae54daa5c2262f3229862b3b
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-31 18:57:24 -0500

    Logica de web scraping añadida en la app scraping

--- django-backend/scraping/views.py
+++ django-backend/scraping/views.py
@@ -1,1 +1,22 @@
 from django.http import HttpResponse
+from playwright.async_api import async_playwright
+from asgiref.sync import sync_to_async
+
+@sync_to_async
+async def scrape_async():
+     async with async_playwright() as p:
+         browser = await p.chromium.launch()
+         context = await browser.new_context()
+         page = await context.new_page()
+         await page.goto('https://example.com')
+
+         # Realiza el scraping aquí
+         # Por ejemplo, obtener el título de la página
+         title = await page.title()
+
+         await browser.close()
+         return title
+
+def scrape(request):
+    title = scrape_async()
+    return HttpResponse(title)
