0f224d2adb7690e87b9be94fcada2b30489582d6
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-30 21:41:20 -0500

    Cambios añadidos en la app comprobante

--- django-backend/comprobante/models.py
+++ django-backend/comprobante/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db import models
+from pedido.models import Pedido
+from tipo_comprobante.models import TipoComprobante
 
-# Create your models here.
+class Comprobante(models.Model):
+    id_comprobante = models.AutoField(primary_key=True)
+    id_tipo_comprobante = models.ForeignKey(TipoComprobante, on_delete=models.CASCADE)
+    id_pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    importe = models.DecimalField(max_digits=10, decimal_places=2)
