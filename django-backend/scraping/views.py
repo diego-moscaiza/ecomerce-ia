ffbfdf28e622ad4c87684b8b48ebf56b5aede970
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-31 19:17:07 -0500

    Lógica de web scraping modificada

--- django-backend/scraping/views.py
+++ django-backend/scraping/views.py
@@ -1,26 +1,38 @@
 from django.http import HttpResponse
-from playwright.async_api import async_playwright # type: ignore
+from playwright.async_api import async_playwright
 import asyncio
+
 
 async def scrape_async():
     async with async_playwright() as p:
-        browser = await p.chromium.launch()
+        browser = await p.chromium.launch(headless=False)
         context = await browser.new_context()
         page = await context.new_page()
-        await page.goto('https://example.com')
+        await page.goto(
+            "https://simple.ripley.com.pe/calzado/zapatillas/urbanas?s=mdco"
+        )
 
-        # Realiza el scraping aquí
-        # Por ejemplo, obtener el título de la página
-        title = await page.title()
+        # Obtener todos los elementos con la clase "catalog-product-details"
+        prendas = await page.eval_on_selector_all(
+            ".catalog-product-details",
+            """
+            (products) => products.map((product) => {
+                const title = product.querySelector(".catalog-product-details__name").innerText.trim();
+                return { title };
+            })
+            """,
+        )
 
         await browser.close()
-        return title
+        return prendas
+
 
 async def scrape(request):
-    title = await scrape_async()
-    return HttpResponse(title)
+    prendas = await scrape_async()
+    return HttpResponse(str(prendas))
+
 
 # Otra forma de hacerlo utilizando asyncio.run()
 def scrape(request):
-    title = asyncio.run(scrape_async())
-    return HttpResponse(title)
+    prendas = asyncio.run(scrape_async())
+    return HttpResponse(str(prendas))
