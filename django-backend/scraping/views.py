2bfb64328b9cfbf899d0ca1edec8c9255f8be870
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-31 19:49:10 -0500

    Lógica para evaluar la disponibilidad de 3 navegadores agregada y lógica para obtener la clase desde una variable añadida

--- django-backend/scraping/views.py
+++ django-backend/scraping/views.py
@@ -2,19 +2,31 @@ from django.http import HttpResponse
 from playwright.async_api import async_playwright
 import asyncio
 
-
 async def scrape_async():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
-        context = await browser.new_context()
+
+        if await p.chromium.launch() is not None:
+            browser = await p.chromium.launch(headless=False)
+        elif await p.msedge.launch() is not None:
+            browser = await p.msedge.launch(headless=False)
+        elif await p.firefox.launch() is not None:
+            browser = await p.firefox.launch(headless=False)
+        else:
+            print("No browser available")
+            return None
+
+        browser_available = browser
+        context = await browser_available.new_context()
         page = await context.new_page()
         await page.goto(
             "https://simple.ripley.com.pe/calzado/zapatillas/urbanas?s=mdco"
         )
 
+        etiqueta_inicial: str = ".catalog-product-details"
+
         # Obtener todos los elementos con la clase "catalog-product-details"
         prendas = await page.eval_on_selector_all(
-            ".catalog-product-details",
+            f"{etiqueta_inicial}",
             """
             (products) => products.map((product) => {
                 const title = product.querySelector(".catalog-product-details__name").innerText.trim();
