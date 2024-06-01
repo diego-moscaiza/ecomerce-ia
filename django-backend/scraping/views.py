d7f10c237e339209b161f13fd19e0246414e52bf
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-05-31 19:10:46 -0500

    Lógica de web scraping mejorada

--- django-backend/scraping/views.py
+++ django-backend/scraping/views.py
@@ -1,22 +1,26 @@
 from django.http import HttpResponse
-from playwright.async_api import async_playwright
-from asgiref.sync import sync_to_async
+from playwright.async_api import async_playwright # type: ignore
+import asyncio
 
-@sync_to_async
 async def scrape_async():
-     async with async_playwright() as p:
-         browser = await p.chromium.launch()
-         context = await browser.new_context()
-         page = await context.new_page()
-         await page.goto('https://example.com')
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        context = await browser.new_context()
+        page = await context.new_page()
+        await page.goto('https://example.com')
 
-         # Realiza el scraping aquí
-         # Por ejemplo, obtener el título de la página
-         title = await page.title()
+        # Realiza el scraping aquí
+        # Por ejemplo, obtener el título de la página
+        title = await page.title()
 
-         await browser.close()
-         return title
+        await browser.close()
+        return title
 
+async def scrape(request):
+    title = await scrape_async()
+    return HttpResponse(title)
+
+# Otra forma de hacerlo utilizando asyncio.run()
 def scrape(request):
-    title = scrape_async()
+    title = asyncio.run(scrape_async())
     return HttpResponse(title)
