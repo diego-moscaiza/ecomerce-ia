e45c803e46010a35309aa5c82c243ea0dca6e6f2
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-06-08 21:47:38 -0500

    Se ha modificado la configuración de la app productos

--- django-backend/productos/admin.py
+++ django-backend/productos/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from .models import Producto
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('id_producto', 'nombre', 'descripcion', 'cantidad', 'precio', 'estado', 'proveedor_nombre', 'categoria_nombre', 'estante_nombre')
+    list_display = ('id_producto', 'nombre', 'marca', 'precio', 'descripcion', 'cantidad', 'talla', 'color', 'genero', 'estado', 'nombre_proveedor', 'nombre_subcategoria', 'numero_estante')
 
-    def proveedor_nombre(self, obj):
+    def nombre_proveedor(self, obj):
         return obj.id_proveedor.nombre
 
-    def categoria_nombre(self, obj):
-        return obj.id_categoria.nombre
+    def nombre_subcategoria(self, obj):
+        return obj.id_subcategoria.nombre
 
-    def estante_nombre(self, obj):
-        return obj.id_estante.nombre
+    def numero_estante(self, obj):
+        return obj.id_estante.numero_estante
 
 admin.site.register(Producto, ProductoAdmin)
