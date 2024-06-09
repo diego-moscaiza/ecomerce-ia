3a6569214fe169f0a6a3a6a343442b665e82dffe
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-06-09 17:48:48 -0500

    URL añadidas en la configuración

--- django-backend/admin_project/urls.py
+++ django-backend/admin_project/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("docs/", include_docs_urls(title="API Docs")),
     path("api/clientes/", include("clientes.urls")),
-    path("api/productos/", include("productos.urls")),
-    path("scraping/", include("scraping.urls")),
+    path("api/catalogo/", include("productos.urls")),
 ]
