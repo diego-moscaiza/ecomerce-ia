63b8eff0666fa33b7d083524f617cf5dfe652e98
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-06-11 20:00:30 -0500

    Se ha mejorado la lógica de la api del chatbot

--- tienda-frontend/src/components/ChatBot/voiceflow.js
+++ tienda-frontend/src/components/ChatBot/voiceflow.js
@@ -1,14 +1,24 @@
-(function (d, t) {
+function loadVoiceflowChat() {
+  const d = document;
+  const t = "script";
   const v = d.createElement(t);
   const s = d.getElementsByTagName(t)[0];
+
   v.onload = function () {
-    window.voiceflow.chat.load({
-      verify: { projectID: "665ab424f9872b3c9593002c" },
-      url: "https://general-runtime.voiceflow.com",
-      versionID: "production",
-    });
+    if (window.voiceflow) {
+      window.voiceflow.chat.load({
+        verify: { projectID: "665ab424f9872b3c9593002c" },
+        url: "https://general-runtime.voiceflow.com",
+        versionID: "production",
+      });
+    } else {
+      console.error("Voiceflow no está disponible en window.");
+    }
   };
-  v.src = "https:/cdn.voiceflow.com/widget/bundle.mjs";
-  v.type = "text/javascript";
+
+  v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
+  v.async = true;
   s.parentNode.insertBefore(v, s);
-})(document, "script");
+}
+
+loadVoiceflowChat();
