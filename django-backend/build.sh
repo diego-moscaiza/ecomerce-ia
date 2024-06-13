cd6fc50ed615d76e78996d631b341a2ccb26bf53
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-06-13 00:58:53 -0500

    Cambios en la configuracion

--- django-backend/build.sh
+++ django-backend/build.sh
@@ -8,5 +8,8 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+# Perform application migrations
+python manage.py makemigrations
+
 # Apply any outstanding database migrations
 python manage.py migrate
