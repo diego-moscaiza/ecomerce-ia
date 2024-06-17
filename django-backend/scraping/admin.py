71182da6c866e35858f3a901df1339bec205171a
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-06-17 12:29:59 -0500

    Modificación del modelo de Scraping de ropa, mejoras en la visualización del panel de administrador

--- django-backend/scraping/admin.py
+++ django-backend/scraping/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from .models import InfoPrendasScraping
+
 # Register your models here.
+
+
+class InfoPrendasAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "nombre",
+        "marca",
+        "precio",
+        "talla",
+        "genero",
+        "img",
+    )
+    search_fields = (
+        "nombre",
+        "marca",
+        "precio",
+        "talla",
+        "genero",
+    )
+    list_filter = (
+        "nombre",
+        "marca",
+        "precio",
+        "talla",
+        "genero",
+    )
+
+
+admin.site.register(InfoPrendasScraping, InfoPrendasAdmin)
