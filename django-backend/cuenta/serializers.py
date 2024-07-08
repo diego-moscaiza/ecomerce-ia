701e282089d4971d275dcc6b2e09606fc1709196
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-07-08 12:37:30 -0500

    Se ha modificado el serializer para obtener el password

--- django-backend/cuenta/serializers.py
+++ django-backend/cuenta/serializers.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class CuentaSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name", "username", "email"]
+        fields = ["id", "first_name", "last_name", "username", "email", "password"]
