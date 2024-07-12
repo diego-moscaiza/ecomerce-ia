f9002cdc7d7761fd2d0094ea916ae958c1a2621a
Diego Moscaiza <diego-moscaiza@outlook.com>
2024-07-11 20:07:41 -0500

    Se ha modificado el modelo de Pedido

--- django-backend/pedido/admin.py
+++ django-backend/pedido/admin.py
@@ -6,20 +6,12 @@ class PedidoAdmin(admin.ModelAdmin):
     list_display = (
         "id_pedido",
         "id_cliente",
-        "nombre_cliente",
         "id_producto",
-        "nombre_producto",
         "costo",
         "estado",
     )
-    search_fields = ("nombre_cliente", "nombre_producto", "estado")
-    list_filter = ("id_cliente__nombre", "id_producto__nombre", "estado")
-
-    def nombre_cliente(self, obj):
-        return obj.id_cliente.nombre
-
-    def nombre_producto(self, obj):
-        return obj.id_producto.nombre
+    search_fields = ("id_cliente", "id_producto", "costo", "estado")
+    list_filter = ("id_cliente", "id_producto", "estado")
 
 
 admin.site.register(Pedido, PedidoAdmin)
--- django-backend/pedido/models.py
+++ django-backend/pedido/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-from clientes.models import Cliente
+from django.contrib.auth.models import User
 from productos.models import Producto
 
 class Pedido(models.Model):
     id_pedido = models.AutoField(primary_key=True)
-    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, blank=True, null=True)
+    id_cliente = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE, blank=True, null=True)
     costo = models.DecimalField(max_digits=10, decimal_places=2)
     estado = models.CharField(max_length=1)
+
+    def __str__(self):
+        return str(self.id_pedido)
